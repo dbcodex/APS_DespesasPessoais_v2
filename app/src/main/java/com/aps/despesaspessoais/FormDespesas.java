f9ea3f13b802b4c5dbb7a2aef7d81753d913a3bc
dbcodex <benner.rock@gmail.com>
2022-11-21 23:34:17 -0300

    Botao Cancelar

--- app/src/main/java/com/aps/despesaspessoais/FormDespesas.java
+++ app/src/main/java/com/aps/despesaspessoais/FormDespesas.java
@@ -2,9 +2,16 @@ package com.aps.despesaspessoais;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FormDespesas extends AppCompatActivity {
+
+    private Button btCancelar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +19,19 @@ public class FormDespesas extends AppCompatActivity {
         setContentView(R.layout.activity_form_despesas);
 
         getSupportActionBar().hide();
+        IniciarComponentes();
+
+        btCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new  Intent(FormDespesas.this, TelaPrincipal.class);
+                startActivity((intent));
+                finish();
+            }
+        });
+    }
+
+    private void IniciarComponentes(){
+        btCancelar = findViewById(R.id.btCancelar);
     }
 }
